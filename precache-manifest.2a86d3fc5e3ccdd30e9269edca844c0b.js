e217413fe6af4aa9ef2855ed428ce29c039c4a70
Bryan <windsor.bryan@gmail.com>
2019-04-14 17:06:47 -0400

    Updates

---
+++ precache-manifest.2a86d3fc5e3ccdd30e9269edca844c0b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "266fb23565d2671c867d450f5ff23ae8",
+    "url": "/react-gh-pages/static/media/WebDeveloperPhoto.266fb235.jpg"
+  },
+  {
+    "revision": "95d796d0c596dfbf075d",
+    "url": "/react-gh-pages/static/css/main.13ad3648.chunk.css"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "a2885d83ec055edcca7e",
+    "url": "/react-gh-pages/static/js/2.9429a728.chunk.js"
+  },
+  {
+    "revision": "160ca8515b36e1641fc5bc61995ce698",
+    "url": "/react-gh-pages/static/media/profile-photo-2.160ca851.jpg"
+  },
+  {
+    "revision": "eba5f93814b922a1d919a4a679cd0e6b",
+    "url": "/react-gh-pages/static/media/Waterfall-Climb.eba5f938.jpg"
+  },
+  {
+    "revision": "8de540d570f5a394b7e6dc9e70d8b8b5",
+    "url": "/react-gh-pages/static/media/Thac Ban Gioc Vietnam.8de540d5.jpg"
+  },
+  {
+    "revision": "95d796d0c596dfbf075d",
+    "url": "/react-gh-pages/static/js/main.91fc31a2.chunk.js"
+  },
+  {
+    "revision": "684b61ce129bf3fe0a776b7e7dc33624",
+    "url": "/react-gh-pages/static/media/EducatorImage.684b61ce.png"
+  },
+  {
+    "revision": "6195e8d208a6015a38344c40c9ebdce0",
+    "url": "/react-gh-pages/static/media/Face-Detect-Capture.6195e8d2.PNG"
+  },
+  {
+    "revision": "515f7e6d67ae4e819713ef4b44ac00a5",
+    "url": "/react-gh-pages/static/media/bills-tracker-capture.515f7e6d.PNG"
+  },
+  {
+    "revision": "453d09d53fc852063de6208b14c86cd8",
+    "url": "/react-gh-pages/static/media/Robofriends-capture.453d09d5.png"
+  },
+  {
+    "revision": "b9cad9ab76f2ed19596bbbce130d0f2f",
+    "url": "/react-gh-pages/static/media/pixel-art-capture.b9cad9ab.PNG"
+  },
+  {
+    "revision": "5eedbfe15ea8e94808f28a87d748770d",
+    "url": "/react-gh-pages/static/media/store-checkout-capture.5eedbfe1.PNG"
+  },
+  {
+    "revision": "c3b83886c97e40a5562495a49d7fea6d",
+    "url": "/react-gh-pages/index.html"
+  }
+];
